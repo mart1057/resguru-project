da3f2d5a7e9b0005a77d05f38ef02dd4aff27dad
mart1057 <81488092+mart1057@users.noreply.github.com>
2023-10-05 03:02:34 +0700

    'update'

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,13 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Prompt';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #003765;
 }
+
 
 nav {
   padding: 30px;
