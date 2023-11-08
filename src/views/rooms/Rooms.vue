4057b103fda3b0b1f4351414cf8d86052eeca553
thisistee <nuttapol.kpn@gmail.com>
2023-11-08 15:14:20 +0700

    expense and room

--- src/views/rooms/Rooms.vue
+++ src/views/rooms/Rooms.vue
@@ -125,16 +125,18 @@
                 <div class="bg-[white] rounded-[16px] flex justify-between p-[14px] h-[160px] cursor-pointer"
                     @click="routeTo('/room-detail')" v-for="data in room">
                     <div class="flex">
-                        <div class="w-[136px] h-[100%] rounded-[22px] bg-[#8396A6]"></div>
+                        <div class="w-[136px] h-[100%] rounded-[22px] bg-[#003765]">
+                           
+                        </div>
                         <div class="ml-[14px]">
 
                             <div class="text-[18px] font-bold text-[#141629]">ห้อง {{ data.attributes.RoomNumber }}</div>
-                            <div class="text-[14px] mt-[12px] font-bold text-[#003765]">{{
+                            <!-- <div class="text-[14px] mt-[12px] font-bold text-[#003765]">{{
                                 data.attributes.user_sign_contract.data ?
                                 data.attributes.user_sign_contract.data.attributes.users_permissions_user.data.attributes.firstName
                                 : "" }} {{ data.attributes.user_sign_contract.data ?
-        data.attributes.user_sign_contract.data.attributes.users_permissions_user.data.attributes.lastName
-        : "" }}</div>
+                                data.attributes.user_sign_contract.data.attributes.users_permissions_user.data.attributes.lastName
+                            : "" }}</div> -->
 
                         </div>
                     </div>
