97a494ee221e5efd872502822c5e55aaa1c72065
mart1057 <81488092+mart1057@users.noreply.github.com>
2023-12-18 00:22:26 +0700

    'update'

--- src/views/rooms/Rooms.vue
+++ src/views/rooms/Rooms.vue
@@ -404,7 +404,7 @@ export default {
         },
         getRoom(code) {
             const loading = this.$vs.loading()
-            fetch('https://api.resguru.app/api/getRoom?buildingid=' + this.$store.state.building + '&buildingFloor=' + this.filter.floor)
+            fetch('https://api.resguru.app/api/getRoom?buildingid=' + this.$store.state.building + '&buildingFloor=' + this.filter.floor+'&roomStatus=Available')
                 .then(response => response.json())
                 .then((resp) => {
                     if (code == 8) {
