2065fdc3cffb656fa6fb4cd1628a4664c818665e
Nonta-w <nontadaech.w@gmail.com>
2025-05-22 07:20:09 +0700

    new log out

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,15 +3,15 @@
         <div class="bg-[#003765] w-[40%] rounded-r-[40px]">
             <div class="flex flex-col justify-between w-[100%] h-[100vh!important] ">
                 <div class="pl-[50px] pt-[30px]">
-                    <div class="text-[white] flex mt-[16px] cursor-pointer" @click="logoutTo()">
-                        <div class="flex justify-center items-center mr-[8px]"><svg width="7" height="12" viewBox="0 0 7 12"
-                                fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M0.338101 5.99962C0.338101 5.88552 0.357334 5.77495 0.3958 5.6679C0.434267 5.56085 0.500293 5.46053 0.593876 5.36695L5.10348 0.857324C5.24834 0.712458 5.42142 0.642591 5.6227 0.647724C5.82398 0.652841 5.99706 0.727841 6.14193 0.872724C6.28679 1.01759 6.35923 1.19322 6.35923 1.39962C6.35923 1.60602 6.28679 1.78166 6.14193 1.92652L2.06883 5.99962L6.1573 10.0881C6.30217 10.233 6.37204 10.406 6.36693 10.6073C6.36179 10.8086 6.28679 10.9817 6.14193 11.1265C5.99706 11.2714 5.82142 11.3438 5.615 11.3438C5.4086 11.3438 5.23297 11.2714 5.0881 11.1265L0.593876 6.6323C0.500293 6.53872 0.434267 6.44 0.3958 6.33615C0.357334 6.23232 0.338101 6.12014 0.338101 5.99962Z"
-                                    fill="white" />
-                            </svg></div>
-                        <div class="flex justify-center items-center">Logout</div>
-                    </div>
+                    <!-- new logout botton-->
+                    <button @click="logoutTo()" class="flex items-center px-6 py-3 bg-gray-800 hover:bg-gray-700 text-white text-sm font-semibold rounded-xl transition-colors duration-200 cursor-pointer shadow-lg">
+                        <div class="flex justify-center items-center mr-3">
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9 21H5C4.44772 21 4 20.5523 4 20V4C4 3.44772 4.44772 3 5 3H9M16 17L21 12M21 12L16 7M21 12H9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                        <span>ออกจากระบบ</span>
+                    </button>
                 </div>
                 
                 <div class="flex flex-col justify-center items-center">
